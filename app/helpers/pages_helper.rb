273f044e0a872dd719aa81c19ef125e8c4481363
Greg Sutliff <gregsutliff@gmail.com>
2014-12-18 07:51:09 -0600

    navigation added

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+
+	def full_title(page_title = '')
+		base_title = "816 Horseshoe Bend Road"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+
 end
